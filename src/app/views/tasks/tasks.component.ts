7e4aacdea4827cfff064eced2204e921726e9247
Yosip Mike Colin <colinparrado@gmail.com>
2024-09-27 14:15:51 -0500

    Filtering tasks

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
+import { DataTask } from 'src/interfaces';
 
 @Component({
   selector: 'app-tasks',
@@ -7,10 +8,37 @@ import { ApiService } from 'src/app/api.service';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent {
+  tasks: DataTask[] = [];
+  type: string = 'completed';
+
   constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.getTasks();
+  }
 
   async getTasks() {
     const data = await this.apiService.getTasks();
-    console.log('data', data);
+    this.tasks = data;
+  }
+
+  async updatedTasks(idTask: number) {
+    try {
+      await this.apiService.updateTask(idTask);
+      await this.getTasks();
+    } catch (error) {
+      alert('Error al actualizar la tarea');
+    }
+  }
+
+  changeView(type: 'completed' | 'incomplete') {
+    this.type = type;
+  }
+
+  getIncompleteTasks() {
+    return this.tasks.filter((item) => item.task.completed === false);
+  }
+  getCompletedTasks() {
+    return this.tasks.filter((item) => item.task.completed === true);
   }
 }
